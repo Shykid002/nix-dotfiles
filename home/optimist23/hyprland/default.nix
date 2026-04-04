8b8fbd8c50c756282381b05071b76b9b2de87015
Shykid002 <shykid002@gmail.com>
2026-04-04 22:17:56 +0100

    added hyprland borderangle animation

--- home/optimist23/hyprland/default.nix
+++ home/optimist23/hyprland/default.nix
@@ -53,8 +53,9 @@ in
         gaps_in = 6;
         gaps_out = 12;
         border_size = 3;
-        "col.active_border" = "$primary";
-        "col.inactive_border" = "$surface";
+         # Use two colors here (e.g., your primary and a secondary/accent)
+        "col.active_border" = "$primary $surface 45deg"; 
+        "col.inactive_border" = "$surface";        
 
              };
 
@@ -92,11 +93,12 @@ in
           enabled = "yes";
           bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
           animation = [
-            "windows, 1, 4, myBezier"
-            "windowsOut, 1, 4, default, popin 80%"
-            "border, 1, 3, default"
-            "fade, 1, 4, default"
-            "workspaces, 1, 4, default"
+            "windows, 1, 8, myBezier, slide left"
+            "windowsOut, 1, 8, default, popin 80%"
+            "border, 1, 8, default"
+            "fade, 1, 8, default"
+            "workspaces, 1, 8, default"
+            "borderangle, 1, 50, default, loop"
           ];
 
 };
