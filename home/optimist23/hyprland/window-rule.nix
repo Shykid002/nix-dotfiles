dec45a214582597d580570c5d2bd8f40f0b89d13
Shykid002 <shykid002@gmail.com>
2026-04-05 13:38:13 +0100

    added window-rule to pomodoro

--- home/optimist23/hyprland/window-rule.nix
+++ home/optimist23/hyprland/window-rule.nix
@@ -11,6 +11,10 @@ wayland.windowManager.hyprland.settings = {
     "size 300 250, match:title ^(Pomodoro Timer)$"
     "center on, match:title ^(Pomodoro Timer)$"
     "pin on, match:title ^(Pomodoro Timer)$"
+
+    "float on, match:class ^(gnome-pomodoro)$, match:title ^(Pomodoro)$"
+    "size 400 500, match:class ^(gnome-pomodoro)$, match:title ^(Pomodoro)$"
+    "center on, match:class ^(gnome-pomodoro)$, match:title ^(Pomodoro)$"
   ];
 };
 
