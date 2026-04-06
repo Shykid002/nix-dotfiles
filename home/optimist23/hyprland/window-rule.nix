605f13b93d490b473cf1e133a9b00204a162a4e7
Shykid002 <shykid002@gmail.com>
2026-04-06 12:34:30 +0100

    window-rule

--- home/optimist23/hyprland/window-rule.nix
+++ home/optimist23/hyprland/window-rule.nix
@@ -7,20 +7,20 @@
 
 wayland.windowManager.hyprland.settings = {
   windowrule = [
-     # Match first, then Effect
-    "match:title ^(Pomodoro Timer)$, float on"
-    "match:title ^(Pomodoro Timer)$, size 300 250"
-    "match:title ^(Pomodoro Timer)$, center on"
-    "match:title ^(Pomodoro Timer)$, pin on"
+    "float on, match:title ^(Pomodoro Timer)$"
+    "size 300 250, match:title ^(Pomodoro Timer)$"
+    "center on, match:title ^(Pomodoro Timer)$"
+    "pin on, match:title ^(Pomodoro Timer)$"
 
-    "match:class ^(gnome-pomodoro)$ match:title ^(Pomodoro)$, float on"
-    "match:class ^(gnome-pomodoro)$ match:title ^(Pomodoro)$, size 400 500"
-    "match:class ^(gnome-pomodoro)$ match:title ^(Pomodoro)$, center on"
+    "float on, match:class ^(waypaper)$, match:title ^(wayland)$"
+    "size 400 500, match:class ^(waypaper)$, match:title ^(waypaper)$"
+    "center on, match:class ^(waypaper)$, match:title ^(waypaper)$"
 
-    "match:title ^(waypaper)$, float on"
-    "match:title ^(waypaper)$, size 300 250"
-    "match:title ^(waypaper)$, center on"
-    "match:title ^(waypaper)$, pin on"
+    "float on, match:class ^(gnome-pomodoro)$, match:title ^(Pomodoro)$"
+    "size 400 500, match:class ^(gnome-pomodoro)$, match:title ^(Pomodoro)$"
+    "center on, match:class ^(gnome-pomodoro)$, match:title ^(Pomodoro)$"
+
+    
   ];
 };
 
