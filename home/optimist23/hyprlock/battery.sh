c41349966601828ca7c69d34a331ef1b2046767f
Shykid002 <shykid002@gmail.com>
2026-04-22 17:13:00 +0100

    updated

--- home/optimist23/hyprlock/battery.sh
+++ home/optimist23/hyprlock/battery.sh
@@ -6,8 +6,8 @@ battery_percentage=$(cat /sys/class/power_supply/$BAT/capacity)
 battery_status=$(cat /sys/class/power_supply/$BAT/status)
 
 # Icons
-battery_icons=("  " "  " "  " "  " "  " "  " "  " "  " "  " "  " "  ")
-charging_icon="  "
+battery_icons=( "󱢠 " "󰛞 " "󰛞 " "󰛞 " "󰛞 " "󰛞 " "󰛞 " "󰋑 " "󰋑 " "󰋑 " )
+charging_icon="󰋠 "
 
 # Get index (0-10)
 icon_index=$((battery_percentage / 10))
@@ -17,4 +17,5 @@ battery_icon=${battery_icons[$icon_index]}
 
 # Output for Hyprlock
 echo "$battery_icon $battery_percentage%"
+
 
